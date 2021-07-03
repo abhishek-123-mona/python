46bd2c2816b250380bf5a5d82c57a5438e1686b8
Joker <abhisehksharma25801@gmail.com>
2021-07-03 14:19:26 +0530

    updates

--- Practice/Pracitce5.py
+++ Practice/Pracitce5.py
@@ -6,11 +6,5 @@ spiral=turtle.Turtle()
 t.bgcolor("black")
 col=("Yellow","white","sky blue","red")
 c=0
-for i in range(50):
-    spiral.forward(i*10)
-    spiral.right(144)
-    spiral.color(col[c])
-    if c==3:
-        c=0
-    else:
-        c+=1
+ 
+        
